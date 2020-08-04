82ab04202708a856825b41dacbf073c5ce2ed1df
Kedrick06 <bfloblk@gmail.com>
2020-08-04 12:13:25 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+  const [questionOne, setAnswerOne] = useState([]);
+  const [questionTwo, setAnswerTwo] = useState([]);
+  const [questionThree, setAnswerThree] = useState([]);
+  const [questionFour, setAnswerFour] = useState([]);
+  const [questionFive, setAnswerFive] = useState([]);
+  const [questionSix, setAnswerSix] = useState([]);
+  const [questionSeven, setAnswerSeven] = useState([]);
+
+  const fetchData = () => {
+    const questionAPI = 'http://anapioficeandfire.com/api/characters/16';
+    const questionAPI2 = 'http://www.anapioficeandfire.com/api/houses/378';
+    const questionAPI3 = 'http://www.anapioficeandfire.com/api/houses/229';
+    const questionAPI4 = 'http://www.anapioficeandfire.com/api/houses/17';
+    const questionAPI5 = 'http://www.anapioficeandfire.com/api/characters/901';
+    const questionAPI6 = 'http://www.anapioficeandfire.com/api/houses/362';
+    const questionAPI7 = 'http://www.anapioficeandfire.com/api/characters/232';
+
+    const getAnswer = axios.get(questionAPI)
+    const getAnswer2 = axios.get(questionAPI2)
+    const getAnswer3 = axios.get(questionAPI3)
+    const getAnswer4 = axios.get(questionAPI4)
+    const getAnswer5 = axios.get(questionAPI5)
+    const getAnswer6 = axios.get(questionAPI6)
+    const getAnswer7 = axios.get(questionAPI7)
+
+    axios.all([getAnswer, getAnswer2, getAnswer3, getAnswer4, getAnswer5, getAnswer6, getAnswer7])
+    .then(
+      axios.spread((...allData) => {
+        const answerNumber1 = allData[0].data.born;
+        const answerNumber2 = allData[1].data.region;
+        const answerNumber3 = allData[2].data.coatOfArms;
+        const answerNumber4 = allData[3].data.seats[1];
+        const answerNumber5 = allData[4].data.aliases[2];
+        const answerNumber6 = allData[5].data.founder;
+        const answerNumber7 = allData[6].data.povBooks[[]];
+
+        setAnswerOne(answerNumber1)
+        setAnswerTwo(answerNumber2)
+        setAnswerThree(answerNumber3)
+        setAnswerFour(answerNumber4)
+        setAnswerFive(answerNumber5)
+        setAnswerSix(answerNumber6)
+        setAnswerSeven(answerNumber7)
+   
+
+      })
+    )
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Question one is: {questionOne}
+        Question two is: {questionTwo}
+        Question three is: {questionThree}
+        Question four is: {questionFour}
+        Question five is: {questionFive}
+        Question six is: {questionSix}
+        Question seven is: {questionSeven}
+
+
     </div>
-  );
+  )
 }
 
 export default App;
